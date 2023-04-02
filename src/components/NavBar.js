ceba425603f32adb53b80f7ecd35dfa7101d11e0
Tharisha Perera <tharishaperera@gmail.com>
2023-04-03 00:29:54 +0530

    logo added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,41 @@
 import Link from 'next/link'
 import React from 'react'
+import Logo from './Logo'
+import { useRouter } from 'next/router'
+import GithubIcon from './Icons'
+
+const CustomLink = ({ href, title, className = "" }) => {
+    const router = useRouter();
+    return (
+        <Link href={href} className={`${className} relative group`}>
+            {title}
+            <span className={`h-[2px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${router.asPath === href ? 'w-full' : 'w-0'}`}>&nbsp;</span>
+        </Link>
+    )
+}
 
 const NavBar = () => {
-  return (
-    <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
-        <nav>
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
-            <Link href="/projects">Projects</Link>
-            <Link href="/blog">Blog</Link>
-        </nav>
-        <h2>Logo</h2>
-        <nav>
-            <Link href="/" target={"_blank"}>T</Link>
-            <Link href="/" target={"_blank"}>T</Link>
-            <Link href="/" target={"_blank"}>T</Link>
-            <Link href="/" target={"_blank"}>T</Link>
-            <Link href="/" target={"_blank"}>T</Link>
-        </nav>
-    </header>
-  )
+    return (
+        <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
+            <nav>
+                <CustomLink href="/" title="Home" className='mr-4' />
+                <CustomLink href="/about" title="About" className='mx-4' />
+                <CustomLink href="/projects" title="Projects" className='mx-4' />
+                <CustomLink href="/blog" title="Blog" className='ml-4' />
+            </nav>
+
+            <nav>
+                <Link href="/" target={"_blank"}><GithubIcon/></Link>
+                <Link href="/" target={"_blank"}>T</Link>
+                <Link href="/" target={"_blank"}>T</Link>
+                <Link href="/" target={"_blank"}>T</Link>
+                <Link href="/" target={"_blank"}>T</Link>
+            </nav>
+            {/* <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+                <Logo />
+            </div> */}
+        </header>
+    )
 }
 
 export default NavBar
