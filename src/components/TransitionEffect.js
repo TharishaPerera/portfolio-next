d931f46e20526139462f5dc2c0de8b80008f79c3
Tharisha Perera <tharisha.perera@cammsgroup.com>
2023-04-10 17:36:49 +0530

    edited transition colors

--- src/components/TransitionEffect.js
+++ src/components/TransitionEffect.js
@@ -5,7 +5,7 @@ const TransitionEffect = () => {
   return (
     <>
       <motion.div
-        className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-dark"
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-dark dark:bg-light"
         initial={{ x: "100%", width: "100%" }}
         animate={{ x: "0%", width: "0%" }}
         exit={{x:["0%","100%"], width:["0%","100%"]}}
@@ -13,14 +13,14 @@ const TransitionEffect = () => {
       />
 
       <motion.div
-        className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-light"
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-light dark:bg-dark"
         initial={{ x: "100%", width: "100%", opacity:1 }}
         animate={{ x: "0%", width: "0%", opacity:0 }}
         transition={{delay:0.2, duration: 0.8, ease: "easeInOut" }}
       />
 
       <motion.div
-        className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-dark"
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-dark dark:bg-light"
         initial={{ x: "100%", width: "100%", opacity:1 }}
         animate={{ x: "0%", width: "0%", opacity:0 }}
         transition={{delay:0.4,  duration: 0.8, ease: "easeInOut" }}
